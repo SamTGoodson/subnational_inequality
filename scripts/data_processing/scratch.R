4721bfe078163f08fa68f7bed66bb28e34ce0078
SamTGoodson <samtgoodson@gmail.com>
2023-08-17 20:53:22 -0400

    first commit

---
+++ scripts/data_processing/scratch.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(rio)
+library(janitor)
+library(manifestoR)
+library(here)
+
+nuts_national_path<-here('data','raw','national_election','eu_ned_joint_nuts2.csv')
+nuts_national<-read.csv(nuts_national_path)
+crosswalk<-read.csv('C:/Users/samtg/github/subnational_inequality/data/raw/crosswalks/ches_pfid_manifesto.csv')
